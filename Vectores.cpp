3c9fd8588837dd6611a19593c748a470adcff965
Tatiana-MQ <tamendezquiros@gmail.com>
2025-05-17 14:26:54 -0600

    cambios 3

--- Vectores.cpp
+++ Vectores.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main() {
-    
+    cout << "la suma es: " << a + b << endl;
+    cout << "la resta es: " << a - b << endl;
+    cout << "la multiplicacion es: " << a * b << endl;
+    cout << "la division es: " << a / b << endl;
 
     return 0;
 }
