b68984c4aee813ef78b280bdb1404d5b80404104
amy10q <amymorataya10@gmail.com>
2025-05-17 14:51:39 -0600

    Commit divi

--- Vectores.cpp
+++ Vectores.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int Resta (int a, int b){
-    cout << "Ingresa el primer numero a multiplicar: ";
+    cout << "Ingresa el primer numero a dividir: ";
     cin >> a;
 
-    cout << "Ingresa el segundo numero a multiplicar: ";
+    cout << "Ingresa el segundo numero a dividir: ";
     cin >> b;
 
-    return a * b;
+    return a / b;
 }
