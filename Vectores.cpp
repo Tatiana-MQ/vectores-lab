22ce20ae90e41152ed3d6766137f5b4943fc6761
amy10q <amymorataya10@gmail.com>
2025-05-17 14:30:59 -0600

    Funcion resta

--- Vectores.cpp
+++ Vectores.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main (
-    cout << "Hola Mundo" << endl;
+int Resta (int a, int b){
+    cout << "Ingresa el primer numero a restar: ";
+    cin >> a;
 
+    cout << "Ingresa el segundo numero a restar: ";
+    cin >> b;
 
-    return 0;
-)
+    return a - b;
+}
