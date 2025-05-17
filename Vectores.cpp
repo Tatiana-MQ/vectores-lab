bbd13a53d3b2998daddcbf4e7080831fdf87ae5a
Tatiana M <tamendezquiros@gmail.com>
2025-05-17 15:04:07 -0600

    Update Vectores.cpp

--- Vectores.cpp
+++ Vectores.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int suma (int a, int b) {
-    return a + b;
     cout << "ingrese el primer numero" << endl;
     cin >> a;
     cout << "ingrese el segundo numero" << endl;
     cin >> b;
+    return a + b;
 }
