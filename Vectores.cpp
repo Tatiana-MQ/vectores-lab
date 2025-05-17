2d88ea12655784a1faf55da5397ebdfc7d3c7b53
Tatiana-MQ <tamendezquiros@gmail.com>
2025-05-17 14:25:39 -0600

    cambios 3

--- Vectores.cpp
+++ Vectores.cpp
@@ -1,3 +1,8 @@
-#include <iostream>
-using namespace std;
 
+suma (int a, int b) {
+    return a + b;
+    cout << "ingrese el primer numero" << endl;
+    cin >> a;
+    cout << "ingrese el segundo numero" << endl;
+    cin >> b;
+}
