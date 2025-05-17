b445fe25d1355a7930c34f7082e7c17ceae13946
Tatiana-MQ <tamendezquiros@gmail.com>
2025-05-17 15:29:01 -0600

    cambio resta

--- Vectores.cpp
+++ Vectores.cpp
@@ -40,7 +40,7 @@ int divi (int a, int b){
 
 int main() {
     cout << "la suma es: " << suma << endl;
-    cout << "la resta es: " << resta << endl;
+    cout << "la resta es: " << Resta << endl;
     cout << "la multiplicacion es: " << Multi << endl;
     cout << "la division es: " << divi<< endl;
 
