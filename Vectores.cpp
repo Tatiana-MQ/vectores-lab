280b82e6c848f405c4d4004a761e3ce96de73cbb
Tatiana-MQ <tamendezquiros@gmail.com>
2025-05-17 14:20:24 -0600

    commit principal

--- Vectores.cpp
+++ Vectores.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 using namespace std;
 
-<<<<<<< HEAD
-int main (
+int main() {
     
-=======
-int main () {
->>>>>>> c5189cc1890c89d3b5fa3accff13049bebce7fd2
 
-   return 0; 
+    return 0;
 }
